aca29b1739ec4f6bb46fc8d4b04365e2ee7ba66a
Pavel Sorokin <nerro@users.noreply.github.com>
2020-09-06 02:21:16 +0200

    Add plugin.xml configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.changelog.closure
+import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+  id("org.jetbrains.changelog") version "0.4.0"
   id("org.jetbrains.intellij") version "0.4.22"
   id("org.jetbrains.kotlin.jvm") version "1.4.0"
 }
@@ -11,7 +14,7 @@ val pluginSinceBuild: String by project
 val pluginUntilBuild: String by project
 val platformVersion: String by project
 
-group = "eu.nerro.codeowners"
+group = "nerro.codeowners"
 description = "IntelliJ CODEOWNERS"
 version = pluginVersion
 
@@ -41,5 +44,17 @@ tasks {
     version(pluginVersion)
     sinceBuild(pluginSinceBuild)
     untilBuild(pluginUntilBuild)
+
+    pluginDescription(closure {
+      File("./README.md").readText().lines().run {
+        val start = "<!-- Plugin description start -->"
+        val end = "<!-- Plugin description end -->"
+
+        if (!containsAll(listOf(start, end))) {
+          throw GradleException("Plugin description section not found in README.md file:\n$start ... $end")
+        }
+        subList(indexOf(start) + 1, indexOf(end))
+      }.joinToString("\n").run { markdownToHTML(this) }
+    })
   }
 }
